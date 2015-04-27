8b6e7e31d31cb22089c436e4a9185ba99b0b22d3
robertgfthomas@gmail.com <hello@robertakarobin.com>
2015-04-27 15:56:10 -0400

    Tweaked starter code

--- cards.js
+++ cards.js
@@ -1,29 +1,23 @@
-function knuth_shuffle(array){
-  var current_index = array.length;
-  var random_index;
-  var temporary_value;
-  while(current_index > 0){
-    random_index   = Math.floor(Math.random() * current_index);
-    current_index -= 1;
+var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 
-    temporary_value       = array[current_index];
-    array[current_index]  = array[random_index];
-    array[random_index]   = temporary_value;
+function shuffle(array){
+  var currentIndex = array.length;
+  var randomIndex;
+  var temporaryValue;
+  while(currentIndex > 0){
+    randomIndex   = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue       = array[currentIndex];
+    array[currentIndex]  = array[randomIndex];
+    array[randomIndex]   = temporaryValue;
   }
   return array;
 }
 
-function build_deck(){ 
-  var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-  var deck    = []
+function buildDeck(){
 
 // Put some code in here!
-
-}
-
-function high_card(){
-
-// Put some more code in here!
 
 }
