6bda9616a4cd2d425690c13168fb2f5132781721
Damon Emde <damon.emde@gmail.com>
2015-04-28 09:01:20 -0400

    decks on decks

--- cards.js
+++ cards.js
@@ -17,7 +17,63 @@ function shuffle(array){
 }
 
 function buildDeck(){
+  var deck=[];
+  for(var i = 0; i < values.length; i++){
+    for(var j = 0; j < suits.length; j++){
+      var temp_hash ={};
+      temp_hash = {value: values[i], suit: suits[j], score: i+2};
+      deck.push(temp_hash);
+    };
+  };
 
 // Put some code in here!
-
+return deck;
 }
+
+var warDeck = buildDeck();
+var shuffledDeck = shuffle(warDeck);
+// console.log(warDeck);
+
+function highCard(deck) {
+  var playerOne = deck.pop();
+  var playerTwo = deck.pop();
+  console.log(playerOne);
+  console.log(playerTwo);
+  if(playerOne.score > playerTwo.score){
+    console.log('player one won!');
+  } else if (playerOne.score === playerTwo.score) {
+    console.log("Tie");
+  } else {
+    console.log('player two won!');
+  };
+}
+
+// highCard(shuffledDeck);
+
+function playWar(deck) {
+  var playerOneDeck = [];
+  for(var i = 0; i < 26; i++){
+    card = deck.pop();
+    playerOneDeck.push(card);
+  }
+  var playerTwoDeck = deck;
+  // console.log(playerOneDeck.length);
+  // console.log(playerTwoDeck.length);
+  while (playerOneDeck.length > 0 || playerTwoDeck.length){
+    var playerOneCard = playerOneDeck.shift();
+    var playerTwoCard= playerTwoDeck.shift();
+    console.log(playerOneCard);
+    console.log(playerTwoCard);
+    if(playerOneCard.score > playerTwoCard.score){
+      console.log('player one won!');
+      playerOneDeck.push(playerTwoCard);
+    } else if (playerOneCard.score === playerTwoCard.score) {
+      console.log("Tie");
+    } else {
+      console.log('player two won!');
+      // playerTwoDeck.push(playerOneCard);
+    };
+  };
+}
+
+playWar(shuffledDeck);
