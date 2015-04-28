4feb06d3a6cd5cd8e6148388c791f44a55220a6d
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-27 23:08:48 -0400

    w07d01hw

--- cards.js
+++ cards.js
@@ -17,7 +17,66 @@ function shuffle(array){
 }
 
 function buildDeck(){
+  var deck = [];
+  for(i=0; i<values.length; i++){
+    for(j=0; j<suits.length; j++){
+      var card = {
+        value: values[i],
+        suit: suits[j],
+        rank: i
+        }
+      deck.push(card);
+    }
+  }
 
+  return deck;
 // Put some code in here!
+
+}
+
+console.log(buildDeck(values, suits));
+
+console.log(shuffle(buildDeck(values, suits)));
+
+
+
+
+function high_card(){
+  var shuffledDeck = shuffle(buildDeck(values, suits));
+  var player1Card = {
+    suit: shuffledDeck[0]["suit"],
+    value: shuffledDeck[0]["value"],
+    rank: shuffledDeck[0]["rank"]
+  }
+  var player2Card = {
+    suit: shuffledDeck[1]["suit"],
+    value: shuffledDeck[1]["value"],
+    rank: shuffledDeck[1]["rank"]
+  }
+  console.log("Player one is dealt a " + player1Card.value + " of " + player1Card.suit);
+  console.log("Player two is dealt a " + player2Card.value + " of " + player2Card.suit);
+
+  if (player1Card.rank === player2Card.rank) {
+    console.log("It's a tie");
+  }else if (player1Card.rank > player2Card.rank) {
+    console.log("Player one wins");
+  }else if (player1Card.rank < player2Card.rank) {
+    console.log("Player two wins");
+  }
+
+}
+
+high_card();
+
+function gameOfWar(){
+  var startDeck = shuffle(buildDeck(values, suits)));
+  var player1Hand = startDeck.splice(0,24);
+  var player2Hand = startDeck.splice(24);
+
+  var round = {
+    player1Hand
+  }
+
+
 
 }
