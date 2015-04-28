2e5ad7b46762c620c767fd0a8043ceb108532ca9
theeltahir <theeltahir@gmail.com>
2015-04-27 21:31:47 -0400

    Solution so far

--- cards.js
+++ cards.js
@@ -17,7 +17,80 @@ function shuffle(array){
 }
 
 function buildDeck(){
+  deck = []
+  for(s in suits){
+    for(v in values){
+      val = values[v];
+      suit = suits[s];
+      var card = {
+        card: val + suit.charAt(0),
+        suit: suit,
+        value: val,
+        strength: parseInt(v,10) //Cast to integer since for i in arr will make i a string
+      };
 
-// Put some code in here!
+      deck.push(card)
+    }
+  }
+  return shuffle(deck);
+}
+
+function highCard(card1, card2){
+  console.log(card1.card, 'vs', card2.card)
+  if (card1.strength > card2.strength)
+    return 1;
+  if (card1.strength < card2.strength)
+    return 2;
+  return 0;
+}
+
+function dumpArray (target, source){
+  while (source.length > 0){
+    target.push(source.shift());
+  }
+}
+
+deck = buildDeck();
+// // This is my first iteration
+// while (deck.length > 0){
+//   player1 = deck.shift();
+//   player2 = deck.shift();
+
+//   console.log(player1.card, 'vs', player2.card);
+//   console.log("The winner is Player:" , highCard(player1, player2));
+// }
+
+//war game stuff
+roundPot = [];
+p1 = [];
+p2 = [];
+
+while (deck.length > 1){
+  p1.push(deck.shift());
+  p2.push(deck.shift());
+}
+
+while(p1.length && p2.length){
+  player1Draw = p1.shift();
+  player2Draw = p2.shift();
+  roundPot.push(player1Draw, player2Draw);
+  winner = highCard(player1Draw, player2Draw);
+  if (winner == 1) {
+    console.log("Player 1 is the winner");
+    dumpArray(p2, roundPot);
+  }
+  else if (winner == 2){
+    console.log("Player 2  is the winner");
+    dumpArray(p1, roundPot);
+  }
+  else{
+    console.log("This around is a tie");
+    for(var x = 0; x <=2; x++)
+      roundPot.push(p1.shift(),p2.shift());
+    console.log("size of put now", roundPot.length);
+  }
 
 }
+console.log("Player 1 Deck Length", p1);
+console.log("Player 2 Deck Length", p2);
+
