e89f1430baf854327042467f0b4e12a1ebca561f
Phillip <phil.winston2@gmail.com>
2015-04-28 09:04:34 -0400

    w07d01 hw

--- cards.js
+++ cards.js
@@ -1,6 +1,6 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
-
+var deck = [];
 function shuffle(array){
   var currentIndex = array.length;
   var randomIndex;
@@ -13,11 +13,37 @@ function shuffle(array){
     array[currentIndex]  = array[randomIndex];
     array[randomIndex]   = temporaryValue;
   }
-  return array;
+  return(array);
 }
+
 
 function buildDeck(){
 
-// Put some code in here!
+for(var i = 0; i < suits.length; i++){
+  for(var x = 0; x < values.length; x++){
+    deck.push(suits[i] + ' ' + values[x]);
+    }
+  }
+    return deck
+}
+
+function highcard() {
+player1 = deck.pop();
+player2 = deck.pop();
+console.log(player1 + ' ' + player2);
+
+  if (player1 > player2) {
+    console.log('player 1 wins ' + player1);
+  } else if (player2 > player1) {
+    console.log('player 2 wins ' + player2);
+  } else if (player2 === player1) {
+    console.log('player 1 and player 2 tie!');
+  }
 
 }
+
+buildDeck();
+
+shuffle(deck);
+
+highcard();
