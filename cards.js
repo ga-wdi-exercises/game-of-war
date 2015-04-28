6372989096c00a3e9c354655e14be09d1e2bdd6e
htylerbrooks <htb5qh@gmail.com>
2015-04-28 08:43:25 -0400

    W07D01 HW

--- cards.js
+++ cards.js
@@ -16,8 +16,61 @@ function shuffle(array){
   return array;
 }
 
-function buildDeck(){
 
-// Put some code in here!
+function buildDeck(){
+  nuevoDeck = [];
+  var cardRank = 0;
+  for (valueIndex in values) {
+    for (suitIndex in suits) {
+      nuevoDeck.push({number:values[valueIndex], suit:suits[suitIndex], rank:cardRank});
+    }
+    cardRank++;
+  }
+  return nuevoDeck;
+}
+
+function showDeck(deck){
+  for (index in deck){
+    console.log(deck[index]);
+  }
+}
+
+function dealCard(){
+  return shuffledDeck.pop();
+}
+
+function showPlayersCard(player){
+  console.log(player.number + " of " + player.suit);
+}
+
+
+function highCard(){
+  var player1 = dealCard();
+  console.log("Player 1's Card is:");
+  showPlayersCard(player1);
+
+  var player2 = dealCard();
+  console.log("Player 2's Card is:");
+  showPlayersCard(player2);
+
+  if (player1.rank > player2.rank) {
+    console.log("Player 1 is the Winner!");
+  }
+
+  else if (player1.rank < player2.rank){
+    console.log("Player 2 is the Winner!");
+  }
+
+  else {
+    console.log("Player 1 and Player 2 tie!");
+  }
 
 }
+
+deck = buildDeck();
+
+shuffledDeck = shuffle(deck);
+
+showDeck(shuffledDeck);
+
+highCard();
