91e628f0b98c2325a2afed27fc8ce85450563e37
Toby <toby.schaeffer@gmail.com>
2015-04-28 06:49:55 -0400

    initial push

--- cards.js
+++ cards.js
@@ -1,3 +1,15 @@
+//
+// This may look familiar...
+//
+// Write all your code in the existing cards.js file. Javascript doesn't natively have an array-shuffling method, unlike Ruby, so one has been included for you.
+//
+// First, modify the build_deck function so that it returns a complete deck of cards.
+//
+// Then, modify the high_card function so that it deals two players a card from the top of a shuffled deck. The computer should be playing against itself since, also unlike Ruby, Node doesn't have an built-in way of getting user input.
+//
+// Print out to the console the two cards, and which player had the higher card (or if it was a tie).
+
+
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 
@@ -16,8 +28,47 @@ function shuffle(array){
   return array;
 }
 
-function buildDeck(){
-
-// Put some code in here!
-
+var deck = []
+function buildDeck(suits, values){
+  for(var index_v in values){
+    for(var index_s in suits){
+      var card = {suit: suits[index_s], value: values[index_v]};
+      deck.push(card);
+    }
+  }
+  return deck
 }
+
+// split deck between two players
+var player1 = []
+var player2 = []
+function dealCards(deck){
+  for(var i=0; i < deck.length; i++){
+    if (i % 2 === 0 ){
+      player1.push(deck[i]);
+    }
+    else{
+      player2.push(deck[i]);
+    }
+  }
+  return deck
+}
+
+//confirm cards are dealt cards out to players
+deck = buildDeck(suits, values)
+shuffledDeck = shuffle(deck)
+dealCards(shuffledDeck)
+console.log("Player 1:  ")
+console.log(player1)
+console.log("Player 2:  ")
+console.log(player2)
+
+
+// // compare cards from two players
+// function highCard(cards){
+//   for(var card in cards){
+//
+//   }
+// }
+
+// Print out to the console the two cards, and which player had the higher card (or if it was a tie).
