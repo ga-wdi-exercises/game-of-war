d85c81332aaef57359d5fc285e897f0915c41af1
Adrian Maseda <amaseda@gmail.com>
2015-04-27 22:26:04 -0400

    Close to final draft. Need to tweak tie game scenario.

--- cards.js
+++ cards.js
@@ -1,5 +1,11 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var tat = 0;
+
+// Initialize deck and player objects
+var gameDeck = buildDeck();
+var playerOne = { deck: gameDeck.slice(0,26) };
+var playerTwo = { deck: gameDeck.slice(26,52) };
 
 function shuffle(array){
   var currentIndex = array.length;
@@ -23,7 +29,93 @@ function buildDeck(){
       deck.push( [values[i], suits[j]] );
     }
   }
-  deck = shuffle( deck );
+  return deck = shuffle( deck );
 }
 
-buildDeck();
+function highCard(){
+  while( playerOne.deck.length > 0 && playerTwo.deck.length > 0 ) {
+
+    console.log( "==========================");
+    console.log( "==========================");
+    console.log( "START OF TURN");
+    console.log( "Player 1 has " + playerOne.deck.length + " cards left." );
+    console.log( "Player 2 has " + playerTwo.deck.length + " cards left." );
+    var oneCard = playerOne.deck.pop();
+    var twoCard = playerTwo.deck.pop();
+    console.log( "--------------------------");
+    console.log( "Player 1 card: " + oneCard );
+    console.log( "Player 2 card: " + twoCard );
+
+    // If Player 1 has the higher card...
+    if ( values.indexOf(oneCard[0]) > values.indexOf(twoCard[0]) ) {
+      console.log( "PLAYER 1 WINS!" );
+      playerOne.deck.unshift( oneCard );
+      playerOne.deck.unshift( twoCard );
+    }
+
+    // If Player 2 has the higher card...
+    else if ( values.indexOf( twoCard[0] ) > values.indexOf( oneCard[0] )) {
+      console.log( "PLAYER 2 WINS!" );
+      playerTwo.deck.unshift( oneCard );
+      playerTwo.deck.unshift( twoCard );
+    }
+
+    // If both cards are the same...
+    else {
+      // if (playerOne.deck[-4] > playerTwo.deck[-4]) {
+      //   for( var i=0; i<4; i++ ) {
+      //     playerOne.deck.unshift( playerTwo.deck.pop() );
+      //   }
+      // }
+      // else if (playerTwo.deck[-4] > playerOne.deck[-4]) {
+      //   for( var i=0; i<4; i++ ) {
+      //     playerTwo.deck.unshift( playerOne.deck.pop() );
+      //   }
+      // }
+      // else {
+      //   // TBD
+      // }
+
+      console.log( "****************************************************************************");
+      // Initialize array to hold eight total cards played by each player in case of tie
+      tempArray = [];
+      for( var i = 0; i < 3; i++ ) {
+        if ( (playerOne.deck.length > 0) || (playerTwo.deck.length > 0) ) {
+          tempArray.push( playerOne.deck.pop() );
+          tempArray.push( playerTwo.deck.pop() );
+        }
+      }
+      // If Player #1 wins tie breaker, put all temp array cards into his deck
+      if ( tempArray[tempArray.length-2] > tempArray[tempArray.length-1] ) {
+        for ( var i = 0; i < tempArray.length; i++ ) {
+          playerTwo.deck.unshift( tempArray[i] );
+        }
+      }
+      // If Player #2 wins tie breaker, put all temp array cards into his deck
+      else if ( tempArray[tempArray.length-1] > tempArray[tempArray.length-2] ) {
+        for ( var i = 0; i < tempArray.length; i++ ) {
+          playerOne.deck.unshift( tempArray[i] );
+        }
+      }
+      // Need to figure out what to do in case of chained ties
+      else if ( tempArray[tempArray.length-2] === tempArray[tempArray.length-1] ) {
+        tat++;
+      }
+    }
+    console.log( "END OF TURN");
+    console.log( "Player 1 has " + playerOne.deck.length + " cards left." );
+    console.log( "Player 2 has " + playerTwo.deck.length + " cards left." );
+  }
+
+  console.log( "==========================");
+  console.log( "==========================");
+  if ( playerOne.deck.length > 0 ) {
+    console.log( "PLAYER 1 WINS!" );
+  }
+  else {
+    console.log( "PLAYER 2 WINS!" );
+  }
+}
+
+highCard();
+console.log("TAT: " + tat);
