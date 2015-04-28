7a4da99f70e7b36542b261069f3d46f4e65ad7cf
luminousbeam <jocelyn.jeriah@gmail.com>
2015-04-27 23:24:00 -0400

    deck of cards .js

--- cards.js
+++ cards.js
@@ -1,10 +1,11 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
 
+
 function shuffle(array){
   var currentIndex = array.length;
   var randomIndex;
-  var temporaryValue;
+ var temporaryValue;
   while(currentIndex > 0){
     randomIndex   = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -16,8 +17,12 @@ function shuffle(array){
   return array;
 }
 
-function buildDeck(){
 
-// Put some code in here!
 
-}
+function buildDeck(n){
+  for(var i = 0; i < n; i ++)
+    for(var j = 0; j < suits.length; j ++)
+      for(var k = 0; k < values.length; k ++)
+          new Card(values[k], suits[j]);
+  return buildDeck();
+  }
