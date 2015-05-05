c1436165c16633d554e30f6fbdac0d408d56a257
luminousbeam <jocelyn.jeriah@gmail.com>
2015-05-04 20:48:26 -0400

    completed cards assignment, minus the bonus

--- cards.js
+++ cards.js
@@ -1,6 +1,8 @@
 var values  = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 var suits   = ["Clubs", "Diamonds", "Hearts", "Spades"];
+var deck = [];
 
+//Shuffle array function
 
 function shuffle(array){
   var currentIndex = array.length;
@@ -17,12 +19,46 @@ function shuffle(array){
   return array;
 }
 
+//Build deck function
 
+function buildDeck(){
 
-function buildDeck(n){
-  for(var i = 0; i < n; i ++)
-    for(var j = 0; j < suits.length; j ++)
-      for(var k = 0; k < values.length; k ++)
-          new Card(values[k], suits[j]);
-  return buildDeck();
-  }
+    for(var i = 0; i < values.length; i++){
+
+        for(var j = 0; j < suits.length; j++){
+
+         deck.push(values[i] + " of " + suits[j]);
+            }
+      }
+    console.log(deck);
+}
+
+buildDeck();
+
+//High card function
+
+function highCard(){
+
+    playerDeck = (shuffle(deck));
+
+    player1 = [];
+    player2 = [];
+
+    player1.push(playerDeck[0]);
+    player2.push(playerDeck[1]);
+
+    console.log("Player one's card: " + player1);
+    console.log("Player two's card: " + player2);
+
+    if (player1[0] > player2[0]) {
+    console.log("Player one has the higher card!");
+    }
+    else if (player1[0] < player2[0]){
+    console.log("Player two has the higher card!");
+    }
+    else {
+      console.log("It's a tie!")
+    }
+}
+
+highCard();
